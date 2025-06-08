b76e548dcbb7ab49a80f7ae90e4a6e7d0a33b738
Matvey Filatov <8matroskinkot8@gmail.com>
2025-06-09 02:48:29 +0300

    Это была жесть

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,51 @@
-import '../scss/style.scss';
-import '../js/menu';
-import '../js/slider';
-import '../js/swiper-bundle';
+import '../scss/style.scss'
+import '../js/swiper-bundle'
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (window.innerWidth <= 768) {
+    const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      centeredSlades: true,
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+        dynamicBullet: true
+      }
+    })
+  }
+
+  const showButton = document.querySelector('.show-more-btn')
+  const brandsGrid = document.querySelector('.brands__grid')
+  const textButton = document.querySelector('.show-more-btn__text')
+  const showIcon = document.querySelector('.show-more-btn__icon')
+
+  showButton.addEventListener('click', function () {
+    brandsGrid.classList.toggle('brands__grid--show')
+    showIcon.classList.toggle('rotate')
+
+    if (brandsGrid.classList.contains('brands__grid--show')) {
+      textButton.textContent = 'Скрыть'
+    } else {
+      textButton.textContent = 'Показать все'
+    }
+  })
+
+  // Управление боковым меню
+  const sidebar = document.querySelector('.sidebar')
+  const burgerBtn = document.querySelector('.header__burger')
+  const closeBtn = document.querySelector('.sidebar__close')
+
+  if (burgerBtn && sidebar) {
+    burgerBtn.addEventListener('click', function () {
+      sidebar.classList.add('open')
+    })
+  }
+
+  if (closeBtn && sidebar) {
+    closeBtn.addEventListener('click', function () {
+      sidebar.classList.remove('open')
+    })
+  }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,111 @@
+// const path = require('path')
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+// const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+// module.exports = {
+//   // Входной файл
+//   entry: ['./src/js/index.js'],
+
+//   // Выходной файл
+//   output: {
+//     filename: './js/bundle.js'
+//   },
+
+//   // Source maps для удобства отладки
+//   devtool: 'source-map',
+
+//   module: {
+//     rules: [
+//       // Транспилируем js с babel
+//       {
+//         test: /\.js$/,
+//         include: path.resolve(__dirname, 'src/js'),
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['@babel/preset-env']
+//           }
+//         }
+//       },
+
+//       // Компилируем SCSS в CSS
+//       {
+//         test: /\.scss$/,
+//         use: [
+//           MiniCssExtractPlugin.loader, // Extract css to separate file
+//           'css-loader', // translates CSS into CommonJS
+//           'postcss-loader', // parse CSS and add vendor prefixes to CSS rules
+//           'sass-loader' // compiles Sass to CSS, using Node Sass by default
+//         ]
+//       },
+
+//       // Подключаем шрифты из css
+//       {
+//         test: /\.(eot|ttf|woff|woff2)$/,
+//         use: [
+//           {
+//             loader: 'file-loader?name=./fonts/[name].[ext]'
+//           }
+//         ]
+//       },
+
+//       // Подключаем картинки из css
+//       {
+//         test: /\.(svg|png|jpg|jpeg|webp)$/,
+//         use: [
+//           {
+//             loader: 'file-loader?name=./static/[name].[ext]'
+//           }
+//         ]
+//       }
+//     ]
+//   },
+//   plugins: [
+//     // Подключаем файл html, стили и скрипты встроятся автоматически
+//     new HtmlWebpackPlugin({
+//       title: 'Webpack 4 Starter',
+//       template: './src/index.html',
+//       inject: true,
+//       minify: {
+//         removeComments: true,
+//         collapseWhitespace: false
+//       }
+//     }),
+
+//     // Кладем стили в отдельный файлик
+//     new MiniCssExtractPlugin({
+//       filename: 'style.css'
+//     }),
+
+//     // Копируем картинки
+//     new CopyWebpackPlugin([
+//       {
+//         from: './src/img',
+//         to: 'img'
+//       }
+//     ])
+//   ]
+// }
+
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  // Входной файл
   entry: ['./src/js/index.js'],
 
-  // Выходной файл
   output: {
-    filename: './js/bundle.js'
+    filename: './js/bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
 
-  // Source maps для удобства отладки
   devtool: 'source-map',
 
   module: {
     rules: [
-      // Транспилируем js с babel
+      // JS
       {
         test: /\.js$/,
         include: path.resolve(__dirname, 'src/js'),
@@ -30,40 +118,53 @@ module.exports = {
         }
       },
 
-      // Компилируем SCSS в CSS
+      // SCSS
       {
         test: /\.scss$/,
         use: [
-          MiniCssExtractPlugin.loader, // Extract css to separate file
-          'css-loader', // translates CSS into CommonJS
-          'postcss-loader', // parse CSS and add vendor prefixes to CSS rules
-          'sass-loader' // compiles Sass to CSS, using Node Sass by default
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../'
+            }
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
         ]
       },
 
-      // Подключаем шрифты из css
+      // Шрифты (обновлённый синтаксис)
       {
         test: /\.(eot|ttf|woff|woff2)$/,
         use: [
           {
-            loader: 'file-loader?name=./fonts/[name].[ext]'
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
           }
         ]
       },
 
-      // Подключаем картинки из css
+      // Изображения (обновлённый синтаксис)
       {
         test: /\.(svg|png|jpg|jpeg|webp)$/,
         use: [
           {
-            loader: 'file-loader?name=./static/[name].[ext]'
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/'
+            }
           }
         ]
       }
     ]
   },
+
   plugins: [
-    // Подключаем файл html, стили и скрипты встроятся автоматически
     new HtmlWebpackPlugin({
       title: 'Webpack 4 Starter',
       template: './src/index.html',
@@ -74,17 +175,8 @@ module.exports = {
       }
     }),
 
-    // Кладем стили в отдельный файлик
     new MiniCssExtractPlugin({
-      filename: 'style.css'
-    }),
-
-    // Копируем картинки
-    new CopyWebpackPlugin([
-      {
-        from: './src/img',
-        to: 'img'
-      }
-    ])
+      filename: 'css/style.css'
+    })
   ]
 }
